8fc9fbb2978bbb83f10d380536958433df6f3a85
OctaviaMelody <makava25@gmail.com>
2022-10-03 15:44:43 +0300

    lab3 закончил

--- kotl1/src/main/kotlin/2lab.kt
+++ kotl1/src/main/kotlin/2lab.kt
@@ -1,72 +1,62 @@
 
 
-fun pizdec(string: String){
-    var x = 0
+fun shift (string: String, num: Int): String{
+    string.map { char -> char + num }.joinToString("")
+    return string
+}
+
+fun cipher(string: String) {
     var num = string.length / 2
-    var str2 = string.removeRange(num, string.length)
-    var str1 = string.removeRange(0, num)
-    var chars1: MutableList<Char> = str1.toMutableList()
+    var str2 = string.removeRange(0, num)
     var chars2: MutableList<Char> = str2.toMutableList()
-    var charsIndex = chars1.indexOf(chars1.find{it == '0'})
-    chars1.set(charsIndex, 'o')
+    var charsIndex2 = chars2.indexOf(chars2.find { it == ' ' })
+    var str1 = string.removeRange(num, string.length)
+    var chars1: MutableList<Char> = str1.toMutableList()
+    var charsIndex = chars1.indexOf(chars1.find { it == '0' })
+    if (charsIndex > -1) {chars1.set(charsIndex, 'o')}
     str1 = chars1.joinToString("")
-    var shiftedString = str1.map {char -> char + 3}.joinToString("")
+    str1 = str1.map { char -> char + 3 }.joinToString("")
     chars1 = str1.toMutableList()
 
-
-    print(shiftedString)
-
-
-
-
-
-
-
-
-
-
-
-    /*while (x < str1.length ){
-        chars1.set(x, chars1.get(x)+3)
-    x++
-    }
-
-    do{
+    do {
         charsIndex = chars1.indexOf(chars1.find { it == '4' })
-        if(charsIndex == -1 )break else{
+        if (charsIndex == -1) break else {
             chars1.set(charsIndex, 'u')
             charsIndex = 11
         }
-    }while(charsIndex != -1)
-    do{
+    } while (charsIndex != -1)
+    do {
         charsIndex = chars1.indexOf(chars1.find { it == '5' })
-        if(charsIndex == -1 )break else{
-        chars1.set(charsIndex, 's')
-        charsIndex = 11
-            }
-    }while(charsIndex != -1)
-
-    x = 0
-    while ( x < str1.length ){
-        chars1.set(x, chars1.get(x)-1)
-        x++
-    }*/
+        if (charsIndex == -1) break else {
+            chars1.set(charsIndex, 's')
+            charsIndex = 11
+        }
+    } while (charsIndex != -1)
+    str1 = chars1.map { char -> char - 1 }.joinToString("")
 
 
 
+    do {
+        charsIndex2 = chars2.indexOf(chars2.find { it == ' ' })
+        if (charsIndex2 == -1) break else {
+            chars2.set(charsIndex2, '_')
+            charsIndex2 = 11
+        }
+    } while (charsIndex2 != -1)
+    //str2 = chars2.joinToString("")
+    str2 = chars2.map { char -> char + 4 }.joinToString("")
+    //str2 = shift(str2,4) попробовал сделать но забил хрен
 
-/*    println(str1)
-    println(str2)*/
-
-
+    println(str1.reversed() + str2.reversed())
 }
 
 
 
 fun main() {
     var str1 = "F2p)v\"y233{0->c}ttelciFc"
-    //var str2 = "Snakebite enter my veins"
-    pizdec(str1)
-    //pizdec(str2)
+    var str2 = "Snakebite enter my veins"
+    cipher(str1)
+    cipher(str2)
+
 
 }
